2bda5e3ca6d969ae765ad1d368e298ebf1926f95
zxxychen <43529983+zxxychen@users.noreply.github.com>
2019-06-17 20:24:36 +0800

    add some funcs

--- K_line.py
+++ K_line.py
@@ -140,6 +140,110 @@ class K_line():
         # fig.savefig(self.code + '.png')
         plt.show(fig)
 
+    def _stock_contain_remove(self):
+        df = self.k_lines.loc[:]
+        rows, cols = df.shape
+
+        # first: clean contain relationships
+        for i in range(rows):
+            # state = 1: rise 2:fall
+            if i == 0:
+                if df.loc[i+1, 'high_b'] > df.loc[i, 'high_b'] and df.loc[i+1, 'low_b'] > df.loc[i, 'low_b']:
+                    state = 1
+                elif df.loc[i+1, 'high_b'] < df.loc[i, 'high_b'] and df.loc[i+1, 'low_b'] < df.loc[i, 'low_b']:
+                    state = 2
+                else:
+                    state = 1
+
+            else:
+                if df.loc[i, 'high_b'] > df.loc[i-1, 'high_b'] and df.loc[i, 'low_b'] > df.loc[i-1, 'low_b']:
+                    #price rise
+                    pass
+                elif df.loc[i, 'high_b'] < df.loc[i-1, 'high_b'] and df.loc[i, 'low_b'] < df.loc[i-1, 'low_b']:
+                    #price fall
+                    pass
+                elif df.loc[i, 'high_b'] < df.loc[i-1, 'high_b'] and df.loc[i, 'low_b'] > df.loc[i-1, 'low_b']:
+                    #contain: i-1 contain i
+                    if state == 1:
+                        df.loc[i-1, 'low_b'] = df.loc[i, 'low_b']
+                    else:
+                        df.loc[i-1, 'high_b'] = df.loc[i, 'high_b']
+                elif df.loc[i, 'high_b'] > df.loc[i-1, 'high_b'] and df.loc[i, 'low_b'] < df.loc[i-1, 'low_b']:
+                    # contain: i contain i-1
+                    if state == 1:
+                        df.loc[i, 'low_b'] = df.loc[i-1, 'low_b']
+                    else:
+                        df.loc[i, 'high_b'] = df.loc[i-1, 'high_b']
+                else:
+                    pass
+        return df
+
+    def _line_plot(self, df):
+        # after contain remove, draw the lines
+        # in line: type: 1:top, 2:bot
+        line = pd.DataFrame(columns=('id', 'price', 'type'))
+        # deal with first line
+        line = line.append(pd.DataFrame({'id':[0], 'price':[df.loc[0, 'open']], 'type':[1]}))
+    
+        rows, cols = self.k_lines.shape
+    
+        for i in range(rows):
+            if i == 0 or i == rows-1:
+                pass
+            else:
+                if df.loc[i, 'high_b'] >= df.loc[i-1, 'high_b'] and df.loc[i, 'high_b'] >= df.loc[i+1, 'high_b']:
+                    line = line.append(pd.DataFrame({'id':[i], 'price':[df.loc[i, 'high_b']], 'type':[1]}), ignore_index=True)
+                elif df.loc[i, 'low_b'] <= df.loc[i-1, 'low_b'] and df.loc[i, 'low_b'] <= df.loc[i+1, 'low_b']:
+                    line = line.append(pd.DataFrame({'id':[i], 'price':[df.loc[i, 'low_b']], 'type':[2]}), ignore_index=True)
+                else:
+                    pass
+    
+        # deal with last line
+        line = line.append(pd.DataFrame({'id':[rows-1], 'price':[df.loc[rows -1, 'close']], 'type':[2]}), ignore_index=True)
+        # line.to_csv('line.csv')
+        cnt = 100
+        while cnt != 0:
+            cnt = 0
+            for i in range(len(line)):
+                if i == 0 or i == len(line) - 1:
+                    pass
+                else:
+                    if line.loc[i, 'type'] == 1:
+                        if line.loc[i + 1, 'type'] == 1:
+                            if line.loc[i+1, 'price'] >= line.loc[i, 'price']:
+                                line.loc[i, 'type'] = 0
+                                cnt += 1
+                            else:
+                                line.loc[i+1, 'type'] = 0
+                                cnt += 1
+                    elif line.loc[i, 'type'] == 2:
+                        if line.loc[i + 1, 'type'] == 2:
+                            if line.loc[i+1, 'price'] <= line.loc[i, 'price']:
+                                line.loc[i, 'type'] = 0
+                                cnt += 1
+                            else:
+                                line.loc[i+1, 'type'] = 0
+                                cnt += 1
+            line = line[~line['type'].isin([0])]
+            line.index = range(len(line))
+    
+        # line.to_csv('line2.csv')
+    
+        return line
+
+
+    def strategy_neck_line_print(self):
+        df = self.k_lines
+        fig = self.fig
+        ax1 = self.ax1
+        main_center_high = 0
+        main_center_low = 0
+
+        df_1 = self._stock_contain_remove()
+        line = self._line_plot(df_1)
+        print(line)
+
+
     def strategy_average_system_test(self):
         # 均线策略 测试
         df = self.k_lines
@@ -227,9 +331,11 @@ if __name__ == "__main__":
     # test.print_k_lines(10)
     test.stock_days_denoise_with_high_and_low()
     test.k_lines_mean()
-    # test.print_k_lines(10)
-    test.candle_plot()
-    test.k_lines_mean_plot()
-    test.strategy_average_system_test()
-    test.k_lines_plot_all()
+    test.print_k_lines(10)
+    # test.candle_plot()
+    # test.k_lines_mean_plot()
+    # test.strategy_average_system_test()
+    # test.k_lines_plot_all()
+    test.strategy_neck_line_print()
+    test.print_k_lines(10)
 
