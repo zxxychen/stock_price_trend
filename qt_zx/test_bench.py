9973612fc52d1af2d4763ef58704a907c8fce9c0
zxxychen <605561605@qq.com>
2020-04-02 21:46:57 +0800

    multiprocessing speed up

--- qt_zx/test_bench.py
+++ qt_zx/test_bench.py
@@ -17,43 +17,49 @@ from Buy_break_bias import Buy_bias, Sell_bias
 from Buy_safe_and_positive_strategy import Buy_safe, Sell_safe
 from Time_work import TimeWorker
 import time
+from multiprocessing import Pool
 
+def run(tscode):
+    buy_factors =[{'xd1':3, 'xd2':20, 'xd3':60, 'class':Buy_safe}]
+    sell_factors=[{'xd1':5, 'xd2':20, 'xd3':60, 'class':Sell_safe, 'ratio':0}]
+    factors = buy_factors + sell_factors
+    stock = K_line(tscode, '20180102', '20201231',get_klines=False)
+    worker = TimeWorker(stock.k_lines[:], factors)
+    worker.fit()
+    profit = worker.draw_trend_profit(ax=None, show=False)
+    print('{} profit is {}'.format(tscode, profit))
+    return profit
 if __name__ == "__main__":
     start_time = time.time()
 
     k_line_list = K_line_list.test_list()
 
-    buy_factors =[{'xd1':3, 'xd2':20, 'xd3':60, 'class':Buy_safe}]
-    sell_factors=[{'xd1':5, 'xd2':20, 'xd3':60, 'class':Sell_safe, 'ratio':0}]
-    factors = buy_factors + sell_factors
     aver_profit = 0
     cnt = 0
 
-    for _, row in k_line_list.iterrows():
-        tscode = row['ts_code']
-        stock = K_line(tscode, '20180102', '20201231',get_klines=False)
-        worker = TimeWorker(stock.k_lines[:], factors)
-        worker.fit()
-        profit = worker.draw_trend_profit(ax=None, show=False)
-        print('{} profit is {}'.format(tscode, profit))
-        aver_profit += profit
-        cnt += 1
+    # for _, row in k_line_list.iterrows():
+    #     tscode = row['ts_code']
+    #     stock = K_line(tscode, '20180102', '20201231',get_klines=False)
+    #     worker = TimeWorker(stock.k_lines[:], factors)
+    #     worker.fit()
+    #     profit = worker.draw_trend_profit(ax=None, show=False)
+    #     print('{} profit is {}'.format(tscode, profit))
+    #     aver_profit += profit
+    #     cnt += 1
     
-    aver_profit /= cnt
-    print('final test profit is {}'.format(aver_profit))
+    # aver_profit /= cnt
+    # print('final test profit is {}'.format(aver_profit))
+
+
+
+    tscode_list = k_line_list.ts_code.tolist()
+    # print(tscode_list)
+    p = Pool(4)
+    res = p.map(run, tscode_list)
+    p.close()
+    p.join()
+    print(res)
+    # run(tscode_list[0])
 
     elapsed = time.time() - start_time
     print('test time is {}'.format(elapsed))
-
-    # stock = K_line('002055.SZ', '20180102', '20201231',get_klines=False)
-    
-    # stock.candle_plot()
-    # stock.k_lines_mean_plot([20,5, 60])
-    # # stock.k_lines_BOLL_plot(20)
-    # buy_factors =[{'xd1':3, 'xd2':20, 'xd3':60, 'class':Buy_safe, 'ax':stock.ax1}]
-    # sell_factors=[{'xd1':5, 'xd2':20, 'xd3':60, 'class':Sell_safe, 'ratio':0}]
-    # factors = buy_factors + sell_factors
-    # print(factors)
-    # worker = Timeworker.TimeWorker(stock.k_lines[:], factors)
-    # worker.fit()
-
